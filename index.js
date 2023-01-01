e2c7ff4234b8488884f95bdeb8b333c772c59d95
sinha45 <alfa45leonidas@gmail.com>
2023-01-01 23:52:22 +0600

    server collab with mongodb

---
+++ index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xoxulaj.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+
+    try {
+
+        const productCollection = client.db('emajohn').collection('products')
+
+        app.get('/products', async (req, res) => {
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
+
+            console.log(page, size);
+
+            const query = {}
+            const cursor = productCollection.find(query);
+            const products = await cursor.skip(page * size).limit(size).toArray();
+            const count = await productCollection.estimatedDocumentCount();
+            res.send({ count, products });
+        })
+    }
+
+
+    finally {
+
+    }
+
+}
+
+
+
+run().catch(err => console.error(err))
+
+app.get('/', (req, res) => {
+    res.send('ema john server is running');
+})
+
+app.listen(port, () => {
+    console.log(`ema john running on ${port}`);
+})
